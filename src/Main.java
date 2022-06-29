ecd9a1a9a5b75157558e42a81216b85a28e96b5b
Egidijus <e.grajauskas@gmail.com>
2022-06-29 19:09:37 +0300

    i made some weird changes

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,9 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        ArrayList<Integer> arr = new ArrayList<>();
+
     for (int i = 0; i < 100; i++) {
-        System.out.println(i);
+        System.out.println(i + " va toks skaicius");
     }
 
 
