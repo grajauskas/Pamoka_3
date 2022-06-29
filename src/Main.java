6b2d4dfb14289d0d2a2fe35a798bfa0e72bd6760
Egidijus <e.grajauskas@gmail.com>
2022-06-29 19:05:42 +0300

    init2

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<Integer> arr = new ArrayList<>();
+    for (int i = 0; i < 100; i++) {
+        System.out.println(i);
+    }
+
+
+
+    }
+}
